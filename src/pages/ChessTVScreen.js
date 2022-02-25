ccc2d2103da6455f8865376fdb5792fc4fcef7dd
M-E-7 <ercegom7@gmail.com>
2022-02-25 21:37:39 +0100

    minor change: added onLoad to chessTVScreen.js

--- src/pages/ChessTVScreen.js
+++ src/pages/ChessTVScreen.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/ChessTVScreen.css";
 
 const ChessTV = (props) => {
+  const [isLoading, setIsLoading] = useState(true);
   const source = "https://lichess.org/tv/frame?theme=brown&bg=dark";
+
   return (
-    <div className="chessTVWrapper">
+    <div className="ChessTVScreen__Wrapper">
       <button
         className="ChessTVScreen__BackButton"
         onClick={props.handlers.titleScreenHandler}
       ></button>
+      {isLoading && <h1>Loading...</h1>}
       <iframe
         src={source}
         style={{ width: "100%", maxWidth: "400px", height: "444px" }}
+        onLoad={() => {
+          setIsLoading(false);
+        }}
         allowtransparency="true"
         frameborder="0"
       ></iframe>
